06f8153741494e874f4fc02c9679903ecef5c95e
Sourabh Saini <sourabhsaini@192.168.1.19>
2023-10-30 20:57:59 +0530

    fix:removed unwanted changes

--- flow.md
+++
@@ -1,3 +0,0 @@
-# FIS Credit flows
-
-![Flow Image](https://miro.medium.com/v2/resize:fit:1400/format:webp/1*pce79LRCOOWTJYHbn9rd4w.png)
--- ui/markdown.js
+++
@@ -1,9 +0,0 @@
-fetch(
-  "https://github.com/ONDC-Official/ONDC-FIS-Specifications/blob/draft-loan/README.md"
-)
-  .then((response) => response.text())
-  .then((text) => {
-    const parsedText = JSON.parse(text);
-    const html = marked.parse(parsedText?.payload?.blob?.richText);
-    document.getElementById("markdown-container").innerHTML = html;
-  });
