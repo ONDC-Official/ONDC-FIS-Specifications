11ab439957203d3143fd694ca9d1dfabe3f1b973
Anuj Sharma <anuj.sharma@ondc.org>
2023-12-29 14:58:08 +0530

    feat: added log submission ui

--- ui/log-submission.js
+++ ui/log-submission.js
@@ -1,6 +1,5 @@
 
 function renderDropdownCases(branchname,filteredData){
-  console.log('renderDropdownCases')
   filteredData?.forEach(function (item) {
     var option = document.createElement("option");
     const fileName = item?.split('.md')[0];
@@ -10,14 +9,12 @@ function renderDropdownCases(branchname,filteredData){
 }
 
 function renderCases(branchName,file) {
-  console.log('branchName', branchName, file)
 fetch(
 `https://raw.githubusercontent.com/ondc-official/ONDC-FIS-Specifications/${branchName}/api/components/docs/${file}.md`
 )
 .then((response) => response.text())
 .then((text) => {
 const html = marked.parse(text);
-console.log('html',html)
 document.getElementById("logSubmission-container").innerHTML = html;
 });
 }
