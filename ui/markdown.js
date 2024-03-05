7d84fd9907897cdff9ea7889c2b220023afccd63
Anuj Sharma <anuj.sharma@ondc.org>
2024-03-05 18:49:57 +0530

    fix: xinput render issue

--- ui/markdown.js
+++ ui/markdown.js
@@ -23,8 +23,8 @@ function renderMarkdown(branchName,file) {
       //if mermaid diagram exist
       let createMermaid;
       if(result?.length){
-        const modifiedText = result[0].replace(/mermaid/g, '');
-        createMermaid = await mermaid.render(`main-summary1`, modifiedText)
+        //const modifiedText = result[0].replace(/mermaid/g, '');
+        createMermaid = await mermaid.render(`main-summary1`, result[1])
       }
       const removedMermaidData = text.replace(/```mermaid[\s\S]+?```/g, '');
       const textWithBranchName = removedMermaidData.replace(/branchName/g, branchName);
@@ -41,12 +41,20 @@ function updateFeature() {
 }
 
 function extractTextBetweenBackticks(inputString) {
-  const regex = /```([\s\S]*?)```/g;
-  const matches = inputString.match(regex);
+  // const regex = /```([\s\S]*?)```/g;
+  // const matches = inputString.match(regex);
   
-  if (matches) {
-      return matches.map(match => match.replace(/```/g, ''));
+  // if (matches) {
+  //     return matches.map(match => match.replace(/```/g, ''));
+  // } else {
+  //     return [];
+  // }
+  const regex = /```mermaid\s*(.*?)\s*```/s;
+  const matches = inputString.match(regex);
+
+  if (matches && matches.length >= 2) {
+    return matches
   } else {
-      return [];
+    console.log("No mermaid data found.");
   }
 }
