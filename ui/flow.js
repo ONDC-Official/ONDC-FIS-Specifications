7895830d0716c6c85a8c42ec8915aa7574fb034c
Amit Kumar <amit.kumar@thewitslab.com>
2024-04-09 15:28:18 +0530

    Inside Flow-UI copy code button added

--- ui/flow.js
+++ ui/flow.js
@@ -22,19 +22,20 @@ async function loadSteps(steps) {
     content.id = step.summary;
     content.classList.add("step-content", "p-4");
 
-    var mermaidDiv = document.createElement("description-div");
-    var yamlDiv = document.createElement("description-yaml");
+    var mermaidDiv = document.createElement("div");
+    var yamlDiv = document.createElement("div");
+    yamlDiv.classList.add("code-section");
 
     if (details && details?.length) {
       for (const [innerIndex, detail] of details.entries()) {
-        var mermaidPane = document.createElement("description-mermaid");
+        var mermaidPane = document.createElement("div");
         const { description, mermaid: mermaidGraph } = detail;
         let result;
         if (mermaidGraph) {
           let removeBacktick = mermaidGraph?.replace(/`/g, "");
           result = await mermaid.render(`summary${index}`, removeBacktick);
         }
-        const {svg} = result || ''
+        const { svg } = result || ''
         mermaidPane.innerHTML =
           "<p>" +
           `${innerIndex + 1}) ${description}` +
@@ -46,18 +47,38 @@ async function loadSteps(steps) {
         mermaidDiv.appendChild(mermaidPane);
       }
     }
-    // yamlDiv.innerHTML =
-    //   '<pre class="yaml-content">' +
-    //   (step?.api === "form" ? step.example.value : JSON.stringify(step.example.value, null, 2)) +
-    //   "</pre>";
-    yamlDiv.innerHTML = step?.api === "form" ? '<div>'+'<pre class="yaml-content">'+'<xmp>'+step.example.value+'</xmp>'+'</pre>'+'<div class="flow-forms">'+step.example.value+'</div>'+'</div>'
-      :'<pre class="yaml-content">' +
-       JSON.stringify(step.example.value, null, 2) +
+
+  
+    const copyButton = document.createElement("div");
+    copyButton.textContent = "";
+    copyButton.classList.add("copy-code-button");
+    copyButton.style.backgroundImage = 'url("../icons/icon-copy.png")';
+
+    copyButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      const textArea = document.createElement("textarea");
+      textArea.value = JSON.stringify(step.example.value, null, 2);
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+      copyButton.style.backgroundImage = 'url("../icons/icon-tick.png")';
+      setTimeout(() => {
+        copyButton.style.backgroundImage = 'url("../icons/icon-copy.png")';
+      }, 2000)
+    });
+
+
+    yamlDiv.innerHTML += step?.api === "form" ? '<div>' + '<pre class="yaml-content">' + '<xmp>' + step.example.value + '</xmp>' + '</pre>' + '<div class="flow-forms">' + step.example.value + '</div>' + '</div>'
+      : '<pre class="yaml-content">' +
+      JSON.stringify(step.example.value, null, 2) +
       "</pre>";
     content.innerHTML = "<div>" + "<h3>" + step.summary + "</h3>" + "</div>";
 
     content.appendChild(mermaidDiv);
     content.appendChild(yamlDiv);
+    yamlDiv.appendChild(copyButton);
+
     link.addEventListener("click", function (event) {
       event.preventDefault();
       document.querySelectorAll(".step-item").forEach(function (item) {
@@ -73,6 +94,12 @@ async function loadSteps(steps) {
     contentPane.appendChild(content);
   }
 }
+
+
+
+
+
+
 
 function updateFlow() {
   var flowDropdown = document.getElementById("flow-dropdown");
@@ -100,9 +127,9 @@ async function loadFlow(flowName) {
         let removeBacktick = mermaidGraph?.replace(/`/g, "");
         result = await mermaid.render(`main-summary${index}`, removeBacktick);
       }
-      const {svg} = result || ''
+      const { svg } = result || ''
       mermaidPane.innerHTML =
-        "<p>" + `${index + 1}) ${description}` + "<p>" + "<p>" + (svg|| '') + "<p>";
+        "<p>" + `${index + 1}) ${description}` + "<p>" + "<p>" + (svg || '') + "<p>";
 
       mermaidDiv.appendChild(mermaidPane);
     }
