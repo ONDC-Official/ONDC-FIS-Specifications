1fc60b3be1efaed669d26615aa9fbd7249795e29
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-05-09 11:32:16 +0530

    added changes for md file

--- ui/change-log.js
+++ ui/change-log.js
@@ -3,31 +3,22 @@ function renderChangeLogDropDown(branchname, filteredData) {
   setsDropDown.innerHTML = "";
   filteredData?.forEach(function (item) {
     var option = document.createElement("option");
-    const fileName = item?.split(".docx")[0];
+    const fileName = item?.split(".md")[0];
     option.text = fileName;
     setsDropDown.add(option);
   });
-  renderDocument(branchname, filteredData[0]?.split(".docx")[0]);
-}
-
-function renderDocument(branchName, file) {
-  if (file.endsWith(".md")) renderMDFile(branchName, file);
-
-  const documentDispalyArea = document.getElementById("change-log-container");
-  documentDispalyArea.innerHTML = `
-  <iframe style="width: 100%; height: 500px;" id="something" src="https://docs.google.com/gview?url=https://github.com/ONDC-Official/ONDC-FIS-Specifications/raw/${branchName}/api/components/docs/changeLog/${file}.docx&embedded=true"></iframe>
-  `;
+  renderMDFile(branchname, filteredData[0]?.split(".md")[0]);
 }
 
 function updateChangeLog() {
   var example_set = document.getElementById("change-log-dropdown");
   const selectedOption = document.getElementById("contract-dropdown")?.value;
-  renderDocument(selectedOption, example_set.value);
+  renderMDFile(selectedOption, example_set.value);
 }
 
 function renderMDFile(branchName, file) {
   fetch(
-    `https://raw.githubusercontent.com/ONDC-Official/ONDC-FIS-Specifications/${branchName}/api/components/docs/changeLog/${file}`
+    `https://raw.githubusercontent.com/ONDC-Official/ONDC-FIS-Specifications/${branchName}/api/components/docs/changeLog/${file}.md`
   )
     .then((response) => response.text())
     .then(async (text) => {
