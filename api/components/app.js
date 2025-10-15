8f6c72f54367c337a3cbf9a44604b7b99cd0b75a
Anuj Sharma <anuj.sharma@ondc.org>
2025-10-15 11:14:20 +0530

    feat: added missing tags

--- api/components/app.js
+++ api/components/app.js
@@ -438,24 +438,24 @@ async function getSwaggerYaml(example_set, outputPath) {
     }
 
     //move to separate files
-    // if (!process.argv.includes(SKIP_VALIDATION.enums) && !hasTrueResult) {
-    //   hasTrueResult = await validateEnumsTags(enums, schemaMap);
-    // }
-    // if (!process.argv.includes(SKIP_VALIDATION.tags) && !hasTrueResult) {
-    //   hasTrueResult = await validateTags(tags, schemaMap);
-    // }
+    if (!process.argv.includes(SKIP_VALIDATION.enums) && !hasTrueResult) {
+      hasTrueResult = await validateEnumsTags(enums, schemaMap);
+    }
+    if (!process.argv.includes(SKIP_VALIDATION.tags) && !hasTrueResult) {
+      hasTrueResult = await validateTags(tags, schemaMap);
+    }
 
-    // if (!process.argv.includes(SKIP_VALIDATION.attributes) && !hasTrueResult) {
-    //   hasTrueResult = await validateAttributes(attributes, schemaMap);
-    // }
+    if (!process.argv.includes(SKIP_VALIDATION.attributes) && !hasTrueResult) {
+      hasTrueResult = await validateAttributes(attributes, schemaMap);
+    }
 
-    // if (!process.argv.includes(SKIP_VALIDATION.exampleAttributes) && !hasTrueResult) {
-    //   await validateExamplesAttributes(exampleSets, attributes)
-    // }
+    if (!process.argv.includes(SKIP_VALIDATION.exampleAttributes) && !hasTrueResult) {
+      await validateExamplesAttributes(exampleSets, attributes)
+    }
 
-    // if (process.argv.includes(BUILD.checkAttributes) && !hasTrueResult) {
-    //     await checkAttributes(exampleSets, attributes)
-    // }
+    if (process.argv.includes(BUILD.checkAttributes) && !hasTrueResult) {
+        await checkAttributes(exampleSets, attributes)
+    }
 
     const missingTags = findMissingTags(tags, exampleSets);
     console.log('Missing Tags:', JSON.stringify(missingTags, null, 2));
