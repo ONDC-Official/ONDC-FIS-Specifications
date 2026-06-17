7b2a4fac240560c0632d2c746419f99139bc9621
Anuj Sharma <anuj.sharma@ondc.org>
2026-06-17 13:34:44 +0530

    feat: added GF

--- ui/branches.js
+++ ui/branches.js
@@ -1,10 +1,10 @@
 const BRANCHES = [
-  // {
-  //   code: "release-FIS10-2.1.0",
-  //   name: "Gift Card",
-  //   short_desc: "Gift - Card",
-  //   status: "RELEASED",
-  // },
+  {
+    code: "release-FIS10-2.1.0",
+    name: "Gift Card",
+    short_desc: "Gift - Card",
+    status: "RELEASED",
+  },
   //  {
   //   code: "draft-FIS11-2.0.0",
   //   name: "Recharge",
